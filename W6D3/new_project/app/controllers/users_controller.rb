ba32e9985fc933b58e2e871ceccc32712d305b5e
Ashe <ashe@nyu.edu>
2020-05-15 20:46:32 -0700

    W6D5

--- W6D3/new_project/app/controllers/users_controller.rb
+++ W6D3/new_project/app/controllers/users_controller.rb
@@ -5,23 +5,38 @@ class UsersController < ApplicationController
             user = User.where("username like '%#{params[:query]}%'")
             render json: user
         else
-            users = User.all
-            render json: users
+            @users = User.all
+            # render json: users
+            render 'index' #can use 'index'
         end
     end
 
     def create
-        user = User.new(user_params)
-        if user.save!
-            render json: user
+        @user = User.new(user_params)
+        if @user.save!
+            redirect_to user_url(@user.id)
         else
-            render json: user.errors.full_messages, status: :unprocessable_entity    
+            render :new   
         end
     end
 
+    def new
+        @user = User.new #placeholder
+        render :new
+    end
+
     def show
-        user = User.find(params[:id])
-        render json: user
+        @user = User.find_by(id: params[:id])
+        if @user
+            render :show
+        else
+            redirect_to users_url
+        end
+    end
+
+    def edit
+        @user = User.find_by(id: params[:id])
+        render :edit
     end
 
     def update
@@ -29,7 +44,7 @@ class UsersController < ApplicationController
         if user.update(user_params)
             redirect_to "/users/#{user.id}"
         else
-            render json: user.errors.full_messages
+            render :edit
         end
     end
 
